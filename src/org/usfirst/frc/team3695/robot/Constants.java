2a603eea226e44207155a60585849eb7fe1090b0
wh1ter0se <ipad3000@yahoo.com>
2017-09-16 18:08:16 -0500

    Added constants. Commented out unused imports.

---
+++ src/org/usfirst/frc/team3695/robot/Constants.java
@@ -0,0 +1,5 @@
+package org.usfirst.frc.team3695.robot;
+
+public class Constants {
+
+}
--- src/org/usfirst/frc/team3695/robot/OI.java
+++ src/org/usfirst/frc/team3695/robot/OI.java
@@ -1,8 +1,8 @@
 package org.usfirst.frc.team3695.robot;
 
-import edu.wpi.first.wpilibj.buttons.Button;
+// import edu.wpi.first.wpilibj.buttons.Button;
 
-import org.usfirst.frc.team3695.robot.commands.ExampleCommand;
+// import org.usfirst.frc.team3695.robot.commands.ExampleCommand;
 
 /**
  * This class is the glue that binds the controls on the physical operator
