89f75d744ed7d91c03254f25240b61ffefe1376e
Wh1ter0se <rckreischer@gmail.com>
2018-01-13 23:42:06 -0600

    Added auto enum.

--- src/org/usfirst/frc/team3695/robot/enumeration/Autonomous.java
+++ src/org/usfirst/frc/team3695/robot/enumeration/Autonomous.java
@@ -2,5 +2,10 @@ package org.usfirst.frc.team3695.robot.enumeration;
 
 /** the possible autonomous modes */
 public enum Autonomous {
-	NOTHING;
+	NOTHING,             // do nothing
+	RUN_FOR_IT,          // RUN FOREST RUN (just go straight til telopInit)
+	LEFT_THEN_RUN,       // move left from the center, then turn and run
+	RIGHT_THEN_RUN,      // move right from the center, then turn and run
+	RUN_AND_PLACE_LEFT,  // run forward, then place the cube on the scale to the left
+	RUN_AND_PLACE_RIGHT; // run forward, then place the cube on the scale to the right
 }
