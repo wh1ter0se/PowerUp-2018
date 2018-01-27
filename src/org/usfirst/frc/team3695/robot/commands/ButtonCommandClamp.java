0d635b7fdd4bc6dd7e7c187dd844c5d16b3a8413
Brogan <brogancle@gmail.com>
2018-01-27 12:57:22 -0600

    Move limits from Clamp to Grow because colton

--- src/org/usfirst/frc/team3695/robot/commands/ButtonCommandClamp.java
+++ src/org/usfirst/frc/team3695/robot/commands/ButtonCommandClamp.java
@@ -12,10 +12,6 @@ import edu.wpi.first.wpilibj.command.Command;
 public class ButtonCommandClamp extends Command {
 	
 	Direction direction;
-    DigitalInput lowerPinionLimit;
-    DigitalInput upperPinionLimit;
-    DigitalInput lowerScrewLimit;
-    DigitalInput upperScrewLimit;
 	
     public ButtonCommandClamp() {
         requires(Robot.SUB_CLAMP);
@@ -23,20 +19,11 @@ public class ButtonCommandClamp extends Command {
 
     protected void initialize() {
     	//Robot.SUB_CLAMP.openArms();
-        lowerPinionLimit = new DigitalInput(1);
-        upperPinionLimit = new DigitalInput(2);
-        lowerScrewLimit = new DigitalInput(3);
-        upperScrewLimit = new DigitalInput(4);
     }
 
     protected void execute() {}
 
-    protected boolean isFinished() {
-        return lowerPinionLimit.get() ||
-                upperPinionLimit.get() ||
-                lowerScrewLimit.get() ||
-                upperScrewLimit.get();
-    }
+    protected boolean isFinished() { return false; }
 
     protected void end() {
     	///Robot.SUB_CLAMP.closeArms();
