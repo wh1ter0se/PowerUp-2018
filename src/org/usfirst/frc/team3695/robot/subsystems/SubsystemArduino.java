4b2664af0934f70efe44195c9c4168f8d7a56a0e
Brogan <brogancle@gmail.com>
2018-01-27 13:21:40 -0600

    Set up Arduino Subsystem to react to recieve data and moved random numbers to Arduino Constants

--- src/org/usfirst/frc/team3695/robot/subsystems/SubsystemArduino.java
+++ src/org/usfirst/frc/team3695/robot/subsystems/SubsystemArduino.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team3695.robot.subsystems;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import org.usfirst.frc.team3695.robot.util.ArduinoConstants;
 
 /**
  * Controls for the Arduino
@@ -18,11 +19,17 @@ public class SubsystemArduino extends Subsystem {
         i2c = new I2C(I2C.Port.kOnboard, 4);
         toSend = new byte[1];
     }
-    
-    //Send data through 
+
     private void sendData(int data){
     	toSend[0] = (byte)data;
     	i2c.transaction(toSend, 1, null, 0);
+    }
+
+    private byte[] recieveData(int request, int size){
+        byte[] recieved = new byte[size];
+        toSend[0] = (byte)request;
+        i2c.transaction(toSend, 1, recieved, size);
+        return recieved;
     }
     
     /** 
@@ -34,17 +41,17 @@ public class SubsystemArduino extends Subsystem {
     }
 
     public void sendAllianceColor(DriverStation.Alliance color){
-    	sendData((color == DriverStation.Alliance.Blue ? 1 : 2));
+    	sendData(color == DriverStation.Alliance.Blue ? ArduinoConstants.BLUE : ArduinoConstants.RED);
     }
     
     public void sendStage(){
     	DriverStation game = DriverStation.getInstance();
     	if(game.isAutonomous()){
-    		sendData(3);
+    		sendData(ArduinoConstants.AUTONOMOUS);
     	} else if (game.isOperatorControl()){
-    		sendData(4);
+    		sendData(ArduinoConstants.OPERATOR_CONTROL);
     	} else {
-    		sendData(200);
+    		sendData(ArduinoConstants.DISABLED);
     	}
     }
 }
---
+++ src/org/usfirst/frc/team3695/robot/util/ArduinoConstants.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team3695.robot.util;
+
+public class ArduinoConstants {
+    public static final int
+        BLUE = 1,
+        RED = 2,
+        AUTONOMOUS = 3,
+        OPERATOR_CONTROL = 4,
+        DISABLED = 200;
+}
