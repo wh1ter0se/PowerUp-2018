eb2c3eae2cd88e0da07a887072e2b2382920ef9c
Brogan <brogancle@gmail.com>
2018-01-27 12:54:07 -0600

    Add limit switched to the clamp

--- src/org/usfirst/frc/team3695/robot/commands/ButtonCommandClamp.java
+++ src/org/usfirst/frc/team3695/robot/commands/ButtonCommandClamp.java
@@ -1,9 +1,8 @@
 package org.usfirst.frc.team3695.robot.commands;
 
-import org.usfirst.frc.team3695.robot.Constants;
+import edu.wpi.first.wpilibj.DigitalInput;
 import org.usfirst.frc.team3695.robot.Robot;
 import org.usfirst.frc.team3695.robot.enumeration.Direction;
-import org.usfirst.frc.team3695.robot.util.Util;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -13,6 +12,10 @@ import edu.wpi.first.wpilibj.command.Command;
 public class ButtonCommandClamp extends Command {
 	
 	Direction direction;
+    DigitalInput lowerPinionLimit;
+    DigitalInput upperPinionLimit;
+    DigitalInput lowerScrewLimit;
+    DigitalInput upperScrewLimit;
 	
     public ButtonCommandClamp() {
         requires(Robot.SUB_CLAMP);
@@ -20,12 +23,19 @@ public class ButtonCommandClamp extends Command {
 
     protected void initialize() {
     	//Robot.SUB_CLAMP.openArms();
+        lowerPinionLimit = new DigitalInput(1);
+        upperPinionLimit = new DigitalInput(2);
+        lowerScrewLimit = new DigitalInput(3);
+        upperScrewLimit = new DigitalInput(4);
     }
 
     protected void execute() {}
 
     protected boolean isFinished() {
-        return false;
+        return lowerPinionLimit.get() ||
+                upperPinionLimit.get() ||
+                lowerScrewLimit.get() ||
+                upperScrewLimit.get();
     }
 
     protected void end() {
