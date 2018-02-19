a34ecf2f8c59b2af639685bd25ff7398a02c7c41
Brogan <brogancle@gmail.com>
2018-02-19 14:34:44 -0600

    Flip images from screw since it's a little upside-down right now

--- src/org/usfirst/frc/team3695/robot/Vision.java
+++ src/org/usfirst/frc/team3695/robot/Vision.java
@@ -54,6 +54,7 @@ public class Vision extends IterativeRobot {
     	CvSource outputScrew = CameraServer.getInstance().putVideo("Screw", VisionConstants.CAM_WIDTH, VisionConstants.CAM_HEIGHT);
     	 while (!Thread.interrupted()){
     		 cvsinkScrew.grabFrame(streamImages);
+    		 Core.rotate(streamImages, streamImages, Core.ROTATE_180);
     		 outputScrew.putFrame(streamImages);
     	 }
     }
