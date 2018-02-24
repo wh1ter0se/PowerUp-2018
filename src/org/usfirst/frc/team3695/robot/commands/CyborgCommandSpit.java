1d09f32a43fe81dafb36451e57231b36f6d05f84
Brogan <brogancle@gmail.com>
2018-02-24 12:32:14 -0600

    Restore the button commands eat and spit. Rotate frame image pi radians. Add test for spit to smartdash

--- src/org/usfirst/frc/team3695/robot/commands/CyborgCommandSpit.java
+++ src/org/usfirst/frc/team3695/robot/commands/CyborgCommandSpit.java
@@ -1,10 +1,31 @@
 package org.usfirst.frc.team3695.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc.team3695.robot.Robot;
 
 public class CyborgCommandSpit extends Command {
 
-    protected boolean isFinished() {
-        return false;
+    long runTime;
+    long startTime;
+    public CyborgCommandSpit(long runTime) {
+        requires(Robot.SUB_MANIPULATOR);
+        this.runTime = runTime;
+    }
+
+    protected void initialize() {}
+
+    protected void execute() {
+        startTime = System.currentTimeMillis();
+        while (startTime + runTime >= System.currentTimeMillis()){
+            Robot.SUB_MANIPULATOR.spit();
+        }
+    }
+
+    protected boolean isFinished() { return false; }
+
+    protected void end() {}
+
+    protected void interrupted() {
+        end();
     }
 }
