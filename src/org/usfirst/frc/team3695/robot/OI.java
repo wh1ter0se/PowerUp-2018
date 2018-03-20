01fbf7d18ead7043eb0daffb1fad327ee2ee03db
Brogan <brogancle@gmail.com>
2018-03-19 21:41:45 -0500

    Needed to fix a few things with commands being weird after moving them

--- src/org/usfirst/frc/team3695/robot/OI.java
+++ src/org/usfirst/frc/team3695/robot/OI.java
@@ -5,9 +5,14 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import org.usfirst.frc.team3695.robot.commands.button.ButtonCommandEat;
+import org.usfirst.frc.team3695.robot.commands.button.ButtonCommandHitTheDeck;
 import org.usfirst.frc.team3695.robot.commands.button.ButtonCommandSpit;
+import org.usfirst.frc.team3695.robot.commands.cyborg.CyborgCommandDriveDirect;
+import org.usfirst.frc.team3695.robot.commands.cyborg.CyborgCommandDriveDistance;
+import org.usfirst.frc.team3695.robot.commands.cyborg.CyborgCommandDriveUntilError;
 import org.usfirst.frc.team3695.robot.commands.cyborg.CyborgCommandRotateDegrees;
-import org.usfirst.frc.team3695.robot.commands.toggle.ToggleCommandReverse;
+import org.usfirst.frc.team3695.robot.commands.toggle.*;
 import org.usfirst.frc.team3695.robot.util.Util;
 import org.usfirst.frc.team3695.robot.util.Xbox;
 
@@ -21,33 +26,33 @@ public class OI {
 	public OI() {
 		/// manipulator wheels
 			Button spinIn = new JoystickButton(OPERATOR, Xbox.RB);
-					spinIn.whileHeld(new org.usfirst.frc.team3695.robot.commands.button.ButtonCommandEat());
+					spinIn.whileHeld(new ButtonCommandEat());
 			Button spinOut = new JoystickButton(OPERATOR, Xbox.LB);
 					spinOut.whileHeld(new ButtonCommandSpit());
 		/// manipulator clamp
 			Button toggleClamp = new JoystickButton(OPERATOR, Xbox.A);
-				toggleClamp.toggleWhenActive(new org.usfirst.frc.team3695.robot.commands.toggle.ToggleCommandClamp());
+				toggleClamp.toggleWhenActive(new ToggleCommandClamp());
 		/// candy cane
 			Button toggleHook = new JoystickButton(OPERATOR, Xbox.B);
-				toggleHook.toggleWhenActive(new org.usfirst.frc.team3695.robot.commands.toggle.ToggleCommandHook());
+				toggleHook.toggleWhenActive(new ToggleCommandHook());
 		/// drop the mast
 			Button dropIt = new JoystickButton(OPERATOR, Xbox.X);
-				dropIt.toggleWhenPressed(new org.usfirst.frc.team3695.robot.commands.button.ButtonCommandHitTheDeck());
+				dropIt.toggleWhenPressed(new ButtonCommandHitTheDeck());
 		/// Reversing mode
 			Button toggleReverse = new JoystickButton(DRIVER, Xbox.Y);
 				toggleReverse.toggleWhenPressed(new ToggleCommandReverse());
 		/// Docking mode
 			Button toggleDock = new JoystickButton(DRIVER, Xbox.X);
-				toggleDock.toggleWhenPressed(new org.usfirst.frc.team3695.robot.commands.toggle.ToggleCommandDock());
+				toggleDock.toggleWhenPressed(new ToggleCommandDock());
 		/// Narrow mode
 			Button toggleNarrow = new JoystickButton(DRIVER, Xbox.B);
-				toggleNarrow.whileHeld(new org.usfirst.frc.team3695.robot.commands.toggle.ToggleCommandNarrow());
+				toggleNarrow.whileHeld(new ToggleCommandNarrow());
 		/// To Compress, or Not To Compress. It is now an option.
-			SmartDashboard.putData("Disable Compressor", new org.usfirst.frc.team3695.robot.commands.toggle.ToggleCommandKillCompressor());
+			SmartDashboard.putData("Disable Compressor", new ToggleCommandKillCompressor());
 			
 
 		/// PID
-			SmartDashboard.putData("Kill PID", new org.usfirst.frc.team3695.robot.commands.toggle.ToggleCommandKillPID());
+			SmartDashboard.putData("Kill PID", new ToggleCommandKillPID());
 			SmartDashboard.putNumber("Right Encoder Position", 0);
 			SmartDashboard.putNumber("Left Encoder Position", 0);
 			
@@ -60,9 +65,9 @@ public class OI {
 	    	DriverStation.reportWarning("OI IS INSTANTIATED", false);
 
 	    /// Cyborg command testers
-			SmartDashboard.putData("Drive Direct", new org.usfirst.frc.team3695.robot.commands.cyborg.CyborgCommandDriveDirect(Util.getAndSetDouble("Drive Direct Power", 0)));
-			SmartDashboard.putData("Drive Distance", new org.usfirst.frc.team3695.robot.commands.cyborg.CyborgCommandDriveDistance(Util.getAndSetDouble("Drive Distance Inches", 0), (int) Util.getAndSetDouble("Drive Distance Timeout", 5000)));
-			SmartDashboard.putData("Drive Until Error", new org.usfirst.frc.team3695.robot.commands.cyborg.CyborgCommandDriveUntilError(500,2,.25, 3));
+			SmartDashboard.putData("Drive Direct", new CyborgCommandDriveDirect(Util.getAndSetDouble("Drive Direct Power", 0)));
+			SmartDashboard.putData("Drive Distance", new CyborgCommandDriveDistance(Util.getAndSetDouble("Drive Distance Inches", 0), (int) Util.getAndSetDouble("Drive Distance Timeout", 5000)));
+			SmartDashboard.putData("Drive Until Error", new CyborgCommandDriveUntilError(500,2,.25, 3));
 			SmartDashboard.putData("Rotate Degree", new CyborgCommandRotateDegrees(Util.getAndSetDouble("Rotate Degrees", 0), (int) Util.getAndSetDouble("Rotate Timeout", 5000)));
 	}
 	
