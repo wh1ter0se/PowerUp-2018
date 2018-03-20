d94bc2a9fdbc5e7721a485484f624513ed61a858
Brogan <brogancle@gmail.com>
2018-03-20 08:32:56 -0500

    Add extra documentation to getAndSetDouble so future years aren't confused by it

--- src/org/usfirst/frc/team3695/robot/util/Util.java
+++ src/org/usfirst/frc/team3695/robot/util/Util.java
@@ -7,11 +7,12 @@ import edu.wpi.first.wpilibj.Preferences;
 public class Util {
 	
 	/**
-	 *	kind of self explanatory, but with some spice
-	 *	use this mainly as a get method to retrieve values the user types into the smart dash
-	 *		(the 'Set' part is only in case the value doesn't exist, backup is a default to use and set if it isn't there)
+	 *	Use this mainly as a get method to retrieve values the user types into the smart dash
+	 *	Use the command within an iterative function such as execute in a command to ensure values get updated
+	 *	It is unlikely this will work correctly if not called iteratively
+	 *	(the 'Set' part is only in case the value doesn't exist, backup is a default to use and set if it isn't there)
 	 */
-	static Preferences pref = Preferences.getInstance();
+	private static Preferences pref = Preferences.getInstance();
 	public static double getAndSetDouble(String key, double backup) {
 		if(!pref.containsKey(key)) pref.putDouble(key, backup);
 		return pref.getDouble(key, backup);
